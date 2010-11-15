5f4b17b853a95de4d244a94175ce70e4451327b9
Nikhil Marathe <nsm.nikhil@gmail.com>
2010-11-15 17:07:17 +0530

    Implemented URL fetch API

--- server.js
+++ server.js
@@ -40,12 +40,31 @@ function tempFile(callback) {
 
 function handleUrl(request, response) {
     tempFile(function(stream) {
-        var url = "";
+        var remote = "";
         request.on('data', function(data) {
-            url += data.toString();
+            remote += data.toString();
         });
         request.on('end', function() {
-            var client = http.createClient(80, url);
+            var parts = url.parse(remote);
+            var client = http.createClient(80, parts.host);
+            client.request('GET', parts.pathname || '/', {
+                'host' : parts.host
+              , 'user-agent' : 'FaceDetector'
+            })
+            .on('response', function(remoteresponse) {
+                 remoteresponse.on('data', function(data) {
+                     stream.write(data);
+                 })
+                 .on('end', function() {
+                     stream.on('close', function() {
+                         reply(face.faces(stream.path), response);
+                     });
+                     stream.end();
+                 })
+                 .on('error', function() {
+                     getOut(500, response);
+                 });;
+            }).end();
         });
     });
 };
@@ -53,15 +72,14 @@ function handleUrl(request, response) {
 function handleFile(request, response) {
     tempFile(function(stream) {
         request.on('data', function(data) {
-            console.log("Wrting");
             stream.write(data);
         });
         request.on('error', console.log);
         request.on('end', function() {
-            stream.end();
             stream.on('close', function() {
                 reply(face.faces(stream.path), response);
             });
+            stream.end();
         });
     });
 };
